be2d130d700b4fa75ebce94153028fb4c765bff1
VitalyDemchenko <v.demchenko.fit.122.20@knute.edu.ua>
2020-10-30 14:19:26 +0200

    добавив список товарів і товарообіг

--- .ipynb_checkpoints/data_service.py
+++ .ipynb_checkpoints/data_service.py
@@ -1,3 +1,49 @@
-    """Модуль для роботи з файлами первинних даних
-    -зчитування та вивід на екран
+"""модуль для роботи з файлами первинних даних
+-зчитування та вивід на екран
+"""
+
+
+def get_goods():
+    """повертає список товарів який отримує з файлу 'goods.txt'
+
+    Returns:
+      goods_list:список товарів
     """
+    with open("./data/goods.txt") as goods_file:
+        from_file = goods_file.readlines()
+
+    #накопичувач товарів
+    goods_list = []
+            
+    for line in from_file:
+       
+       #відрізати '\n' в кінці рядка
+       line = line[:-2]
+
+       line_list = line.split(';')
+       goods_list.append(line_list)
+    
+    return goods_list
+
+def show_goods(goods):
+    """виводить на екран список товарів заданого діапазона
+        
+    Args:    
+      goods([list]):список товарів
+    """
+
+    good_code_from = input("З якого кода? ")
+    good_code_to = input("По який код? ")
+    
+    kol_lines = 0
+    
+    for good in goods:
+        if good_code_from <= good[0] <= good_code_to:
+           print("код: {:4} назва: {:15} скидка: {:4}".format(good[0], good[1], good[2]))
+           kol_lines += 1
+    
+    if kol_lines == 0:
+         print("Код не знайдено")
+
+goods = get_goods()
+show_goods(goods)
