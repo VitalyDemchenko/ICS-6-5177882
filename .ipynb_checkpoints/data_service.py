71dfa9ca6001e24948165791d4c8c47aec20b993
VitalyDemchenko <v.demchenko.fit.122.20@knute.edu.ua>
2020-11-06 23:59:03 +0200

    добавив ще 2 функції для 2 таблиці в data_service.py, також змінив завдання 3 з 2 лабораторної

--- .ipynb_checkpoints/data_service.py
+++ .ipynb_checkpoints/data_service.py
@@ -47,3 +47,49 @@ def show_goods(goods):
 
 goods = get_goods()
 show_goods(goods)
+
+
+def get_goods_circulations():
+  """повертає список товарообігу універмагу який отримує з файлу 'goods_circulations.txt'
+
+  Returns:
+    goods_circulations_list:список товарообігу
+  """
+  with open("./data/goods_circulations.txt") as goods_circulations_file:
+      from_file = goods_circulations_file.readlines()
+  
+  #накопичувач товарообігів
+  goods_circulations_list = []
+
+  for line in from_file:
+     
+     #відрізати '\n' в кінці рядка
+     line = line[:-2]
+
+     line_list = line.split(';')
+     goods_circulations_list.append(line_list)
+
+  return goods_circulations_list
+
+def show_goods_circulations(goods_circulations):
+    """виводить на екран список товарообігу заданого діапазона
+
+    Args:
+      goods_circulations ([list]):список товарообігу
+    """
+
+    goods_circulation_code_from = input("З якого кода? ")
+    goods_circulation_code_to = input("По який код? ")
+
+    kol_lines = 0
+
+    for goods_circulation in goods_circulations:
+        if goods_circulation_code_from <= goods_circulation[0] <= goods_circulation_code_to:
+           print("код: {:4} план: {:4} очікуєме виконання: {:4} рік: {:5}".format(goods_circulation[0], goods_circulation[1], goods_circulation[2], goods_circulation[3]))
+           kol_lines += 1
+
+    if kol_lines == 0:
+         print("Код не знайдено")
+
+goods_circulations = get_goods_circulations()
+show_goods_circulations(goods_circulations)
