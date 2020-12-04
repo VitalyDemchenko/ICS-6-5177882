596bd6804a49dcd5ec264595405850af2fe14afc
VitalyDemchenko <v.demchenko.fit.122.20@knute.edu.ua>
2020-12-04 08:33:13 +0200

    Трохи модифіковано незначно process data

--- .ipynb_checkpoints/process_data.py
+++ .ipynb_checkpoints/process_data.py
@@ -60,10 +60,10 @@ def create_gross_income():
         
     return gross_income_list
 
-#gross_incomes = create_gross_income()
+gross_incomes = create_gross_income()
 
-#for item in gross_incomes:
-#    print(item)
+for item in gross_incomes:
+    print(item)
 
 #print(get_goods())
 #print(get_goods_circulations())
