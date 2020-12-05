a808e0dbca44e78278e11bb97f18d14aa414068e
VitalyDemchenko <v.demchenko.fit.122.20@knute.edu.ua>
2020-12-05 19:48:28 +0200

    перероблено структуру process-data

--- .ipynb_checkpoints/process_data.py
+++ .ipynb_checkpoints/process_data.py
@@ -6,13 +6,13 @@ from data_service import get_goods, get_goods_circulations
 #структура запису для вихідних даних
 #gross_income - валовий дохід
 gross_income = {   
-    'goods_group'                                     : "",       #товарна група
-    'year'                                            : "",       #рік
-    'plan_of_commodity_circulations'                  : 0.0,      #план товарообігу
-    'expected_performance_of_commodity_circulations'  : 0.0,      #очікуване виконання товарообігу
+    'name_goods_group'                                : "",       #найменування товарної групи
+    'year'                                            : 0,        #рік
+    'plan_of_commodity_circulations'                  : 0,        #план товарообігу
+    'expected_performance_of_commodity_circulations'  : 0,        #очікуване виконання товарообігу
     'trade_discount'                                  : 0.0,      #торгова скидка
-    'plan_of_gross_income'                            : 0.0,      #план валового доходу
-    'expected_performance_of_gross_income'            : 0.0       #очікуване виконання валового доходу
+    'plan_of_gross_income'                            : 0,        #план валового доходу
+    'expected_performance_of_gross_income'            : 0         #очікуване виконання валового доходу
 }
 
 def create_gross_income():
@@ -22,7 +22,7 @@ def create_gross_income():
         gross_income_list: список валового доходу
     """
     
-    def get_goods_group_name(good_code):
+    def get_name_of_goods_group(good_code):
         """знаходить товар по коду"""
         for good in goods:
             if good_code == good[0]:
@@ -40,19 +40,19 @@ def create_gross_income():
     #накопичувач валового доходу
     gross_income_list = []
 
-    goods_circulations = get_goods_circulations()
     goods = get_goods()
+    goods_circulations = get_goods_circulations()
 
     for goods_circulation in goods_circulations:
         
         #робоча змінна
         gross_income_work = gross_income.copy()
         
+        gross_income_work['name_of_goods_group']                            = get_name_of_goods_group(goods_circulation[0])
+        gross_income_work['trade_discount']                                 = get_trade_discount(goods_circulation[0])
         gross_income_work['year']                                           = goods_circulation[3]
         gross_income_work['plan_of_commodity_circulations']                 = goods_circulation[1]
         gross_income_work['expected_performance_of_commodity_circulations'] = goods_circulation[2]
-        gross_income_work['goods_group']                                    = get_goods_group_name(goods_circulation[0])
-        gross_income_work['trade_discount']                                 = get_trade_discount(goods_circulation[0])
         gross_income_work['plan_of_gross_income']                           = float(gross_income_work['plan_of_commodity_circulations']) * float(gross_income_work['trade_discount']) * 0.01
         gross_income_work['expected_performance_of_gross_income']           = float(gross_income_work['expected_performance_of_commodity_circulations']) * float(gross_income_work['trade_discount']) * 0.01
 
@@ -60,10 +60,10 @@ def create_gross_income():
         
     return gross_income_list
 
-gross_incomes = create_gross_income()
+#gross_incomes = create_gross_income()
 
-for item in gross_incomes:
-    print(item)
+#for item in gross_incomes:
+#    print(item)
 
 #print(get_goods())
 #print(get_goods_circulations())
